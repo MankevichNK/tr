f0c66c3ee3697939887371c3d96d0c46de6a00b0
Mankevich Nadya <nadya.mankevichhh@mail.ru>
2020-02-14 20:40:05 +0300

    Initial Commit

---
+++ tr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tr
+//
+//  Created by Пользователь on 2/14/20.
+//  Copyright © 2020 Пользователь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
